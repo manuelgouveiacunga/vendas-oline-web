578423d4611bdc38ce9ed4d2da1ab7bbb46f3e83
Manuel Domingos <jonathamarielson@gmail.com>
2025-04-21 22:57:00 +0100

    feat: create Login components

--- src/modules/login/screens/LoginScreem.tsx
+++ src/modules/login/screens/LoginScreem.tsx
@@ -1,10 +1,21 @@
-import { BackgroundImage } from '../styles/LoginScreem.styles';
+import { LimitedContainer } from '../styles/LoginScreem.styles';
+import {
+  BackgroundImage,
+  ContainerLogin,
+  ContainerLoginScreem,
+  LogoImage,
+} from '../styles/LoginScreem.styles';
 
 const LoginScreem = () => {
   return (
-    <div>
-      <BackgroundImage src="./background-image.jpg" />
-    </div>
+    <ContainerLoginScreem>
+      <ContainerLogin>
+        <LimitedContainer>
+          <LogoImage src="./logo.jpg" alt="Logo" />
+        </LimitedContainer>
+      </ContainerLogin>
+      <BackgroundImage src="./background.png" alt="Background" />
+    </ContainerLoginScreem>
   );
 };
 
--- src/modules/login/styles/LoginScreem.styles.ts
+++ src/modules/login/styles/LoginScreem.styles.ts
@@ -1,10 +1,37 @@
 import styled from 'styled-components';
 
+export const ContainerLoginScreem = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
 export const BackgroundImage = styled.img`
   position: absolute;
-  left: 0;
   top: 0;
-  wight: 100%;
+  left: 0;
+  width: 100%;
   height: 100vh;
   object-fit: cover;
+  z-index: -1;
+  filter: blur(10px);
+`;
+export const LogoImage = styled.img``;
+
+export const ContainerLogin = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #d9d9d9;
+  border-radius: 20px;
+  width: 100%;
+  height: 100vh;
+  max-width: 646px;
+`;
+
+export const LimitedContainer = styled.div`
+  width: 100%;
+  max-width: 498px;
+  display: flex;
+  justify-content: center;
 `;
