13f5f69e21e5d016f9bbc605755914f7d63a6283
Manuel Domingos <jonathamarielson@gmail.com>
2025-04-27 16:26:34 +0100

    feat: insert state

--- src/modules/login/screens/LoginScreem.tsx
+++ src/modules/login/screens/LoginScreem.tsx
@@ -1,4 +1,5 @@
 import { Input } from 'antd';
+import { useState } from 'react';
 
 import Button from '../../../shared/buttons/button/Button';
 import { TitleInput } from '../../../shared/Inputs/Input/input.styles';
@@ -11,6 +12,19 @@ import {
 } from '../styles/LoginScreem.styles';
 
 const LoginScreem = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+  const handleLogin = () => {
+    alert(`Username: ${username}, Password: ${password}`);
+  };
+
   return (
     <ContainerLoginScreem>
       <ContainerLogin>
@@ -20,10 +34,10 @@ const LoginScreem = () => {
             LOGIN
           </TitleLogin>
           <TitleInput>USUÁRIO</TitleInput>
-          <Input title="USUÁRIO" />
+          <Input title="USUÁRIO" onChange={handleUsername} value={username} />
           <TitleInput>SENHA</TitleInput>
-          <Input title="SENHA" />
-          <Button type="primary" margin="40px 0px 16px 0px">
+          <Input type="password" title="SENHA" onChange={handlePassword} value={password} />
+          <Button type="primary" margin="40px 0px 16px 0px" onClick={handleLogin}>
             ENTRAR
           </Button>
         </LimitedContainer>
